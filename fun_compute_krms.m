96be2b9a9bf85262f6f9116f378d0e50cfffee42
Kévin Martins <kevin.martins@u-bordeaux.fr>
2022-07-01 06:55:33 +0200

    Minor edits to the function computing Boussinesq dominant wavenumbers.

--- fun_compute_krms.m
+++ fun_compute_krms.m
@@ -2,8 +2,8 @@ function [ krms ] = fun_compute_krms( h0 , f , P , B )
 % Compute root-mean square wavenumker k following Herbers et al. (2000), Eq. 12
 %
 % Inputs:
-%   h0 - local water depth
-%   f  - frequency array 
+%   h0 - local water depth [m]
+%   f  - frequency array [Hz]
 %   P  - power spectrum [m^2], not a density
 %   B  - power bispectrum [m^3], not a density
 %
@@ -17,33 +17,33 @@ function [ krms ] = fun_compute_krms( h0 , f , P , B )
   
   % Initialisation
   krms = NaN*f;
-  nmid  = (length(f)-1)/2 + 1; % Middle frequency (f = 0)
-  g     = 9.81;                % Gravity
+  nmid = (length(f)-1)/2 + 1; % Middle frequency (f = 0)
+  g    = 9.806;               % Gravity
   
   % Transforming P and B in densities
   df = abs(f(2)-f(1));
   P  = P/df;
   B  = B/df^2;
   
-  for fi = 1:length(f)
+  for ifn = 1:length(f)
     % Initialisation of the cumulative sum and wavenumbers
-    sumtmp         = 0; 
-    krms(fi)       = 2*pi*f(fi)/sqrt(g*h0); % Linear part
+    sumtmp   = 0; 
+    krms(ifn) = 2*pi*f(ifn)/sqrt(g*h0); % Linear part
     
     % Loop over frequencies
-    for ff = 1:length(f)
-      ifr3 = nmid + (fi-nmid) - (ff-nmid);
-      if (ifr3 >= 1) && (ifr3 <= length(f))
-        sumtmp = sumtmp + df*real(B(ff,ifr3));
+    for ifp = 1:length(f)
+      ifn_p = nmid + (ifn-nmid) - (ifp-nmid);
+      if (ifn_p >= 1) && (ifn_p <= length(f))
+        sumtmp = sumtmp + df*real(B(ifp,ifn_p));
       end
     end
     
     % Non-linear terms
-    Beta_fr = h0*(2*pi*f(fi))^2/(3*g);
-    Beta_am = 3*sumtmp/(2*h0*P(fi));
+    Beta_fr = h0*(2*pi*f(ifn))^2/(3*g);
+    Beta_am = 3*sumtmp/(2*h0*P(ifn));
     
     % Non-linear wavenumber
-    krms(fi) = krms(fi)*sqrt( 1 + Beta_fr - Beta_am);
+    krms(ifn) = krms(ifn)*sqrt( 1 + Beta_fr - Beta_am);
   end
 end
   
