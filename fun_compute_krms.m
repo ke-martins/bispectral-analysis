3743613ae5840760bd8292bd7436abe1352e0092
ke-martins <kevin.martins@cnrs.fr>
2025-01-16 17:02:44 +0100

    working towards v2; adding 4th order frequency terms as an option; other minor editorial edits

--- fun_compute_krms.m
+++ fun_compute_krms.m
@@ -1,49 +1,70 @@
-function [ krms ] = fun_compute_krms( h0 , f , P , B )
-% Compute root-mean square wavenumker k following Herbers et al. (2000), Eq. 12
-%
+function [ krms ] = fun_compute_krms( h0 , f , P , B , varargin )
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Compute the root-mean square wavenumker krms following Herbers et al. (2000, Eq. 12), 
+% based on the Boussinesq theory of Herbers and Burton (1997). In this work, the authors
+% neglect fourth-order frequency terms, which can improve the linear predictions in deeper 
+% water depths. This is an option here.
+% 
 % Inputs:
 %   h0 - local water depth [m]
 %   f  - frequency array [Hz]
 %   P  - power spectrum [m^2], not a density
 %   B  - power bispectrum [m^3], not a density
-%
-% Notes: f, P and B arrays are two-sided in regards to f; f should be centered around 0 Hz
+%   option - 'second' or 'fourth' order frequency dispersion term
 %
 % Outputs: 
 %   krms - root-mean square wavenumker k [1/m], size of input f
 %
-% March 31, 2020
-% Kévin Martins - kevin.martins@u-bordeaux.fr
+% Notes: f, P and B arrays are two-sided with regards to f; f should be centered around 0 Hz
+%
+% Last update on January 15, 2025
+% Kévin Martins - kevin.martins@cnrs.fr
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+  % Checking inputs
+  if nargin < 5
+    order = 'second';
+  elseif nargin == 5
+    order = char(varargin{1});
+    if and(~strcmp(order,'second'),~strcmp(order,'fourth')), error('Wrong input for order of frequency dispersion effects.'); end
+  end  
   
   % Initialisation
-  krms = NaN*f;
   nmid = (length(f)-1)/2 + 1; % Middle frequency (f = 0)
-  g    = 9.806;               % Gravity
+  g    = 9.81;                % Gravity
+  krms = 2*pi*f/sqrt(g*h0);   % Linear, shallow water wavenumber
   
   % Transforming P and B in densities
   df = abs(f(2)-f(1));
   P  = P/df;
   B  = B/df^2;
   
-  for ifn = 1:length(f)
+  % Looping over frequencies
+  for fi = 1:length(f)
     % Initialisation of the cumulative sum and wavenumbers
-    sumtmp   = 0; 
-    krms(ifn) = 2*pi*f(ifn)/sqrt(g*h0); % Linear part
+    sumtmp   = 0;
     
     % Loop over frequencies
-    for ifp = 1:length(f)
-      ifn_p = nmid + (ifn-nmid) - (ifp-nmid);
-      if (ifn_p >= 1) && (ifn_p <= length(f))
-        sumtmp = sumtmp + df*real(B(ifp,ifn_p));
+    for ff = 1:length(f)
+      ifr3 = nmid + (fi-nmid) - (ff-nmid);
+      if (ifr3 >= 1) && (ifr3 <= length(f))
+        sumtmp = sumtmp + df*real(B(ff,ifr3));
       end
     end
     
     % Non-linear terms
-    Beta_fr = h0*(2*pi*f(ifn))^2/(3*g);
-    Beta_am = 3*sumtmp/(2*h0*P(ifn));
+    Beta_fr  = h0*(2*pi*f(fi))^2/(3*g);
+    Beta_fr2 = h0^2*(2*pi*f(fi))^4/(6*g)^2;
+    Beta_am  = 3*sumtmp/(2*h0*P(fi));
     
     % Non-linear wavenumber
-    krms(ifn) = krms(ifn)*sqrt( 1 + Beta_fr - Beta_am);
+    if strcmp(order,'second')
+      krms(fi) = krms(fi)*sqrt( 1 + Beta_fr - Beta_am);
+    else
+      krms(fi) = krms(fi)*sqrt( 1 + Beta_fr + Beta_fr2 - Beta_am);
+    end
   end
+
+  return
 end
   
--- fun_compute_krms_terms.m
+++ fun_compute_krms_terms.m
@@ -1,36 +1,44 @@
 function [ gamma_fr , gamma_fr2 , gamma_am ] = fun_compute_krms_terms( f , P , B )
-% Compute non-linear frequency and amplitude dispersion terms contributing to the 
-% root-mean square wavenumker krms following Herbers et al. (2002).
-% Compared to his Eq. 12, we retain frequency terms at order mu^2, in 
-% order to improve the linear dispersive properties of the Boussinesq approximation.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Compute the non-linear frequency and amplitude dispersion terms contributing to the 
+% root-mean square wavenumker krms following Herbers et al. (2000).
+% Compared to their Eq. 12, we retain frequency terms at order mu^2, in 
+% order to improve the linear dispersive properties in deeper water depth. This function was written for 
+% Boussinesq-based depth inversion applications. See the following reference for more details:
+%
+%    Martins, K., Bonneton, P., de Viron, O., Turner, I. L., Harley, M. D., & Splinter, K. (2023). 
+%    New Perspectives for Nonlinear Depth‐Inversion of the Nearshore Using Boussinesq Theory. 
+%    Geophysical Research Letters, 50(2), e2022GL100498.
 %
 % Inputs:
 %   f  - frequency array [Hz]
 %   P  - power spectrum [m^2], not a density
 %   B  - power bispectrum [m^3], not a density
 %
+% Outputs: 
+%   gamma_fr  - frequency dispersion term, corresponding to beta_fr/h, size of input f
+%   gamma_fr2 - frequency dispersion term, corresponding to beta_fr2/h^2, size of input f
+%   gamma_am  - amplitude dispersion term, corresponding to h*beta_am, size of input f
+%
 % Notes: f, P and B arrays are two-sided in regards to f; f should be centered around 0 Hz
 %
-% Outputs: 
-%   gamma_fr - frequency dispersion term, corresponding to beta_fr/h, size of input f
-%   gamma_fr2 - frequency dispersion term, corresponding to beta_fr2/h^2, size of input f
-%   gamma_am - amplitude dispersion term, corresponding to h*beta_am, size of input f
-%
-% 1 July 2022
-% Kévin Martins - kevin.martins@u-bordeaux.fr
+% March 31, 2020
+% Kévin Martins - kevin.martins@cnrs.fr
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   
   % Initialisation
   gamma_fr  = NaN*f;
   gamma_fr2 = NaN*f;
   gamma_am  = NaN*f;
   nmid      = (length(f)-1)/2 + 1; % Middle frequency (f = 0)
-  g         = 9.806;               % Gravity
+  g         = 9.806;                % Gravity
   
   % Transforming P and B in densities
   df = abs(f(2)-f(1));
   P  = P/df;
   B  = B/df^2;
   
+  % Looping over frequencies
   for fi = 1:length(f)
     % Initialisation of the cumulative sum and wavenumbers
     sumtmp = 0;
@@ -45,8 +53,10 @@ function [ gamma_fr , gamma_fr2 , gamma_am ] = fun_compute_krms_terms( f , P , B
     
     % Non-linear terms
     gamma_fr(fi)  = (2*pi*f(fi))^2/(3*g);
-    gamma_fr2(fi) = (2*pi*f(fi))^4/(6*g)^2;
+    gamma_fr2(fi) = 0*(2*pi*f(fi))^4/(6*g)^2;
     gamma_am(fi)  = 3*sumtmp/(2*P(fi));
   end
+
+  return
 end
   
