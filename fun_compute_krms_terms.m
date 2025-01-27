b7d974223404108cad31024bd8a938f1e4f063b9
ke-martins <kevin.martins@cnrs.fr>
2025-01-27 16:03:51 +0100

    fix bug in frequency indices

--- fun_compute_krms_terms.m
+++ fun_compute_krms_terms.m
@@ -1,4 +1,4 @@
-function [ gamma_fr , gamma_fr2 , gamma_am ] = fun_compute_krms_terms( f , P , B )
+function [ gamma_fr , gamma_fr2 , gamma_am ] = fun_compute_krms_terms( f , P , B , varargin )
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Compute the non-linear frequency and amplitude dispersion terms contributing to the 
 % root-mean square wavenumker krms following Herbers et al. (2000).
@@ -14,47 +14,56 @@ function [ gamma_fr , gamma_fr2 , gamma_am ] = fun_compute_krms_terms( f , P , B
 %   f  - frequency array [Hz]
 %   P  - power spectrum [m^2], not a density
 %   B  - power bispectrum [m^3], not a density
+%   fc - cutoff frequency [Hz], in case the raw timeseries was noisy
 %
 % Outputs: 
 %   gamma_fr  - frequency dispersion term, corresponding to beta_fr/h, size of input f
-%   gamma_fr2 - frequency dispersion term, corresponding to beta_fr2/h^2, size of input f
+%   gamma_fr2 - fourth order frequency dispersion term, corresponding to beta_fr2/h^2, size of input f
 %   gamma_am  - amplitude dispersion term, corresponding to h*beta_am, size of input f
 %
 % Notes: f, P and B arrays are two-sided in regards to f; f should be centered around 0 Hz
 %
-% March 31, 2020
+% January 23, 2025
 % Kévin Martins - kevin.martins@cnrs.fr
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+  % Checking inputs
+  if nargin < 4
+    fc = max(f);
+  elseif nargin == 4
+    fc = varargin{1};
+  end  
   
   % Initialisation
   gamma_fr  = NaN*f;
   gamma_fr2 = NaN*f;
   gamma_am  = NaN*f;
   nmid      = (length(f)-1)/2 + 1; % Middle frequency (f = 0)
-  g         = 9.806;                % Gravity
+  g         = 9.806;               % Gravity
   
   % Transforming P and B in densities
   df = abs(f(2)-f(1));
   P  = P/df;
   B  = B/df^2;
   
-  % Looping over frequencies
-  for fi = 1:length(f)
+  for ifn = 1:length(f)
     % Initialisation of the cumulative sum and wavenumbers
     sumtmp = 0;
     
     % Loop over frequencies
-    for ff = 1:length(f)
-      ifr3 = nmid + (fi-nmid) - (ff-nmid);
-      if (ifr3 >= 1) && (ifr3 <= length(f))
-        sumtmp = sumtmp + df*real(B(ff,ifr3));
+    for ifp = 1:length(f)
+      ifn_p = nmid + (ifn-nmid) - (ifp-nmid);
+      if (ifn_p >= 1) && (ifn_p <= length(f))
+        if and(abs(f(ifn_p)) > df,abs(f(ifp)) > df)
+          sumtmp = sumtmp + df*real(B(ifp,ifn_p));
+        end
       end
     end
     
     % Non-linear terms
-    gamma_fr(fi)  = (2*pi*f(fi))^2/(3*g);
-    gamma_fr2(fi) = 0*(2*pi*f(fi))^4/(6*g)^2;
-    gamma_am(fi)  = 3*sumtmp/(2*P(fi));
+    gamma_fr(ifn)  = (2*pi*f(ifn))^2/(3*g);
+    gamma_fr2(ifn) = (2*pi*f(ifn))^4/(6*g)^2;
+    gamma_am(ifn)  = 3*sumtmp/(2*P(ifn));
   end
 
   return
