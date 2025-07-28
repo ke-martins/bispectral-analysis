2623e38992a7ff989e5694da0bbd2d61f4c3efe8
ke-martins <kevin.martins@cnrs.fr>
2025-07-28 22:17:38 +0200

    making the sign of Snl source term clearer based on bispectrum definition; add comments and remove unused gravity variable

--- fun_compute_Snl.m
+++ fun_compute_Snl.m
@@ -1,9 +1,11 @@
 function [ Snl ] = fun_compute_Snl( h0 , f , B )
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Compute non-linear 'Snl' term for the energy transfer between triads following Herbers and Burton (1997), see their Eq. 14.
+% Compute the source term 'Snl' for the non-linear energy transfers between triads following Herbers and Burton (1997).
+% As discussed in Martins et al. (2021, https://doi.org/10.1016/j.coastaleng.2021.103917), we take the conjugate of the 
+% bispectrum B as computed in this toolbox with fun_compute_bispectrum.m in order to be consistent with their Eq. 14.
 %
 % Inputs:
-%   h0 - local water depth [m]
+%   h0 - local mean water depth [m]
 %   f  - frequency array [Hz]
 %   B  - power bispectrum [m^3], not a density
 %
@@ -12,15 +14,14 @@ function [ Snl ] = fun_compute_Snl( h0 , f , B )
 % Outputs: 
 %   Snl - source term for non-linear energy transfers between triads [m^2], size of input f
 %
-% Last update on January 15, 2025
+% Last update on July 28, 2025
 % Kévin Martins - kevin.martins@cnrs.fr
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   
   % Initialisation
   Snl  = NaN*f;
   nmid = (length(f)-1)/2 + 1; % Middle frequency (f = 0)
-  g    = 9.81;                % Gravity
-  
+
   % Transforming B in densities
   df = abs(f(2)-f(1));
   B  = B/df^2;
@@ -33,13 +34,12 @@ function [ Snl ] = fun_compute_Snl( h0 , f , B )
     for ff = 1:length(f)
       ifr3 = nmid + (fi-nmid) - (ff-nmid);
       if (ifr3 >= 1) && (ifr3 <= length(f))
-        sumtmp = sumtmp + (3*pi*f(fi)/h0)*imag(B(ff,ifr3))*df;
+        sumtmp = sumtmp + (3*pi*f(fi)/h0)*imag(conj(B(ff,ifr3)))*df;
       end
     end
     
-    % Non-linear Source term
+    % Source term for non-linear energy transfers
     Snl(fi) = sumtmp;
   end
-
   return
 end
